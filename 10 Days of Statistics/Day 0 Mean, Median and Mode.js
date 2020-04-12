7fc483d61584e8adf97627511fba1433daff7d2f
lukaszpacula <56814678+lukaszpacula@users.noreply.github.com>
2020-04-12 13:42:56 +0200

    Create Day 0 Mean, Median and Mode.js

---
+++ 10 Days of Statistics/Day 0 Mean, Median and Mode.js
@@ -0,0 +1,34 @@
+function processData(input) {
+input = input.split("\n")[1].split(" ").map(v=>v*1);
+var sum = 0;
+var i = 0;
+var length = input.length;
+for (i;i<length;i++) {
+     sum += input[i];
+    }  
+var mean = sum / length;
+
+console.log(mean.toFixed(1));  // 1st OUTPUT: MEAN
+
+input.sort(function(a,b){return a-b});
+var median = 0;
+if (length % 2 == 0) {
+    median = (input[length/2-1] + input[length/2]) / 2;
+} else {
+    median = input[Math.round(length/2-1)];
+}
+console.log(median.toFixed(1));  // 2nd OUTPUT: MEDIAN
+
+var obj = {};
+var mode = input.forEach(n => {
+    obj[n] ? obj[n]++ : obj[n] = 1});
+var count = 0;
+var val;
+for (let keys in obj) {
+    if (obj[keys] > count) {
+        count = obj[keys];
+        val = keys;
+    }
+  }
+  console.log(val);  // 3rd OUTPUT: MEDIAN
+}
